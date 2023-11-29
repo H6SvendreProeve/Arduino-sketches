3aac1467c0b469a5608fb9e0d2c06c4994f7bbf3
uncharted220501@hotmail.com <uncharted220501@hotmail.com>
2023-11-29 08:59:36 +0100

    oploading mkr1010 connected and get request

---
+++ sketch_apiSlave_nina/config.h
@@ -0,0 +1,6 @@
+
+  #define SSID "H6 Svendeprove grp3"
+  #define PASSWORD "iltsvind"
+
+  #define HOST "192.168.1.113"
+  #define PATH "/oxygen/GetAllOxygenMeasurements"
--- sketch_apiSlave_nina/sketch_apiSlave_nina.ino
+++ sketch_apiSlave_nina/sketch_apiSlave_nina.ino
@@ -1,8 +1,12 @@
 #include <WiFiNINA.h>
+#include "config.h"
 
 char ssid[] = "H6 Svendeprove grp3";
 char pass[] = "iltsvind";
+char host[] = HOST;
+char path[] = PATH;
 int status = WL_IDLE_STATUS;  // Initialize status variable
+
 void setup() {
   // Start serial communication
   Serial.begin(9600);
@@ -21,7 +25,9 @@ void setup() {
 }
 
 void loop() {
-  // Your main code goes here
+  String respones = sendHttpRequsetchar(host, path);
+  Serial.println("printing respones:");
+  Serial.println(respones);
 }
 
 void printWiFiStatus() {
@@ -29,4 +35,33 @@ void printWiFiStatus() {
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
+}
+
+String sendHttpRequsetchar(const char* host, const char* path){
+  String message = "";
+  WiFiClient client;
+    if(client.connect(host, 8088)){
+     Serial.println("Connected to server");
+     
+     client.print("GET " + String(path) + " HTTP/1.1\r\n");
+     client.print("Host: " + String(host) + "\r\n");
+     //client.print("X-Api-Key: " + String(apiKey) + "\r\n");
+     //client.print("X-MasterArduinoId: " + String(masterArduinoId) + "\r\n");
+     client.print("Connection: close\r\n\r\n");
+
+     while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.print(c);
+        message = message + c;
+      }
+    }
+
+    client.stop();
+    Serial.println("Disconnected from server");   
+  } else {
+    Serial.println("Connection failed");
+  }
+  Serial.println("returns");
+  return message;
 }
